2da1ff8b24ac342ae104fe1f9e96d1f534c23e58
Jose COTES <jose.cotes@hotmail.fr>
2021-12-29 09:25:04 +0100

    Commit of 28-09 halfway, modify html/ CSS and insert js

---
+++ script.js
@@ -0,0 +1,9 @@
+const panels = document.querySelectorAll(".container > *");
+
+panels[0].addEventListener("click", function () {
+  console.log("Yeah");
+});
+
+panels[0].addEventListener("click", function () {
+  console.log("Pas Yeah");
+});
