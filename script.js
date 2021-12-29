2f01b68559e437490a0a53b5581b98f5e33f8154
Jose COTES <jose.cotes@hotmail.fr>
2021-12-29 22:18:34 +0100

    Commit of 05/10 almostFinished, transfer <div> pictures html to js css finished

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const panels = document.querySelectorAll(".container > *");
 
-panels[0].addEventListener("click", function () {
-  console.log("Yeah");
-});
+for (let panel of panels) {
+  panel.addEventListener("click", function () {
+    panels.forEach((p) => p.classList.remove("active"));
+    panel.classList.add("active");
+  });
+}
 
-panels[0].addEventListener("click", function () {
-  console.log("Pas Yeah");
-});
+const panelDiv = document.createElement("div");
+
+panelDiv.style = "background-image: url(./images/explore-the-world.jpg)";
+panelDiv.className = "panel active";
+
+console.log(panelDiv);
+console.dir(panelDiv);
+
+// <div
+//       style="background-image: url(./images/explore-the-world.jpg)"
+//       class="panel active"
+//     >
+//       <h3>Explore the World</h3>
+//     </div>
