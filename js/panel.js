8e6306891d5cea76de772b20b498ac1e0f96f9eb
Jose COTES <jose.cotes@hotmail.fr>
2022-01-01 08:58:09 +0100

    Commit of 26/10 almostFinished, js file with functions

--- js/panel.js
+++ js/panel.js
@@ -1,1 +1,30 @@
-class PAnel {}
+export class Panel {
+  constructor(title = "no title") {
+    const div = (this.html = document.createElement("div"));
+    this.initialize(div, title);
+    this.listenClickEvent();
+  }
+
+  initialize(div, title) {
+    // div.style = "background-image: url(images/" + img  + ".jpg)";
+    div.style = "background-image: url(images/city-on-winter.jpg)";
+    div.className = "panel";
+    div.innerHTML = "<h3>" + title + "</h3>";
+  }
+
+  listenClickEvent() {
+    this.html.addEventListener("click", () => {
+      Array.from(this.html.parentNode.children).forEach((p) => {
+        p.classList.remove("active");
+      });
+      this.html.classList.add("active");
+    });
+  }
+
+  static render(panels) {
+    panels[0].html.classList.add("active");
+    panels.forEach((p) => {
+      document.querySelector(".container").appendChild(p.html);
+    });
+  }
+}
--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-const images = [
-  "explore-the-world",
-  "city-on-winter",
-  "mountains-and-clouds",
-  "sunny-beach",
-  "wild-forest",
+import { Panel } from "./js/panel.js";
+
+const panels = [
+  new Panel("city on winter"),
+  new Panel("explore the world"),
+  new Panel("mountains and clouds"),
+  new Panel("sunny beach"),
+  new Panel("wild forest"),
 ];
 
-for (let image of images) {
-  const panel = createPanel(image);
-  panel.addEventListener("click", function () {
-    // panels.forEach(p => p.classList.remove('active'));
-    panel.classList.add("active");
-  });
-}
-
-function createPanel(img) {
-  const panelDiv = document.createElement("div");
-  panelDiv.style = "background-image: url(./images/" + img + ".jpg)";
-  panelDiv.className = "panel";
-  panelDiv.innerHTML = "<h3>MON TITRE</h3>";
-  document.querySelector(".container").appendChild(panelDiv);
-  return panelDiv;
-}
-
-// <div
-//       style="background-image: url(./images/explore-the-world.jpg)"
-//       class="panel active"
-//     >
-//       <h3>Explore the World</h3>
-//     </div>
+Panel.render(panels);
